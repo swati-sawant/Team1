b6681572fe9ad59e3d3ef952f88a9f39fcd78450
Swati Sawant <sssawant@mitaoe.ac.in>
2022-07-25 14:59:30 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import JsonDataDisplay from './MyPractice/GeekTable'
+function App() {
+return (
+	<div className="App">
+	<h1>Hello Geeks!!!</h1>
+	<JsonDataDisplay/>
+
+	</div>
+);
+}
+
+export default App;
